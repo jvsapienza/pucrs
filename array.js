1dfa60bc33b6bdad06a403c06b007c0d955cf5c2
jvsapienza <jvsapienza@gmail.com>
2025-04-01 14:09:33 -0300

    examples

---
+++ array.js
@@ -0,0 +1,20 @@
+/* 
+--array--
+index[0]
+*/
+
+var shopping = ["bread", "milk", "cheese", "hummus", "noodles"];
+console.log(shopping[0])
+
+// modifying array
+shopping[0] = "notbread"
+console.log(shopping);
+
+
+// Finding the lenght
+console.log(shopping.length)
+
+// Acessing and modifying array
+
+// LIFO stack
+// FIFO queue
